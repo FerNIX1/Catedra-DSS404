4fc65fe3584d7bd76e2160f8687daac1fea8b7d3
Edgar Fernando Lopez Zavaleta <90074636+FerNIX1@users.noreply.github.com>
2023-03-26 18:18:19 -0600

    cambio en procesar.php

--- base/procesar.php
+++ base/procesar.php
@@ -1,28 +1,40 @@
 <?php
 function validar(){
-// Paso 1: Conectar a la base de datos
-$usuario=$_POST['usuario'];
-$contra=$_POST['contra'];
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dss_catedra_bdd";
+    // Paso 1: Conectar a la base de datos
+    $usuario=$_POST['usuario'];
+    $contra=$_POST['contra'];
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "dss_catedra_bdd";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-$consulta="SELECT * FROM usuario WHERE Nombre_Usuario='$usuario' and pass='$contra' ";
-$resultado=mysqli_query($conn,$consulta);
-$filas=mysqli_num_rows($resultado);// me da un resultado si coincide de vuelve 1 si hay o 0 si no
-if($filas>0){
-    header('Location: recursos/menuCliente.php');
-    exit;
-}
-else{
-   echo "Usuario o contraseña invalida"; 
-}
-mysqli_close($conn);
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // establecer el modo de error PDO a excepción
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $consulta = "SELECT * FROM usuario WHERE Nombre_Usuario=:usuario and pass=:contra";
+        $stmt = $conn->prepare($consulta);
+        $stmt->bindParam(':usuario', $usuario);
+        $stmt->bindParam(':contra', $contra);
+        $stmt->execute();
+
+        $filas = $stmt->rowCount(); // me da un resultado si coincide de vuelve 1 si hay o 0 si no
+
+        if ($filas > 0) {
+            
+            header('Location: recursos/menuCliente.php');
+            exit;
+        } else {
+            echo "Usuario o contraseña inválida";
+        }
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
 }
 
-if(isset($_POST['enviar'])){
+if (isset($_POST['enviar'])) {
     validar();
 }
 ?>
