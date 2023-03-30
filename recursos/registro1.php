9803f50f271b20564f33a7012703bc4a57c7cf9a
aziel0x8 <aziel1966@gmail.com>
2023-03-29 20:09:47 -0600

    Update registro1.php
    validaciones del lado del servidor

--- recursos/registro1.php
+++ recursos/registro1.php
@@ -1,24 +1,42 @@
 <?php 
 
-
+// viendo el problema de diferentes roles voy a probar una variable de control en la 
+// base de datos
 
 include("conexion.php");
-$dui = trim($_POST['dui']);
-$usuario = trim($_POST['usuario']);
-$correo = trim($_POST['correo']);
-$fecNac =trim($_POST['fecnac']) ;
-$contra =trim($_POST['contra']);
 
-$query ="INSERT INTO `usuario`(`DUI_Usuario`, `Nombre_Usuario`, `Correo_Electronico`, `FechaNacimiento` , `Pass`) VALUES ('$dui','$usuario','$correo','$fecNac','$contra')";
 
-$finconsulta = mysqli_query($conexion,$query);
+if (isset($_POST['dui']) || isset($_POST['usuario']) || isset($_POST['correo']) || isset($_POST['fecnac']) || isset($_POST['contra'])) {
 
-        if ($finconsulta) {
-            echo "<p>Conexion exitosa</p>";
+    $dui = trim($_POST['dui']);
+    $usuario = trim($_POST['usuario']);
+    $correo = trim($_POST['correo']);
+    $fecNac =trim($_POST['fecnac']) ;
+    $contra =trim($_POST['contra']);
+    $contra_encriptada = password_hash($contra, PASSWORD_DEFAULT); //encripta la base de datos
+    if (filter_var($correo, FILTER_VALIDATE_EMAIL) || preg_match('/^[0-9]{8}-[0-9]{1}$/', $dui)) {
+        
+        $query ="INSERT INTO `usuario`(`DUI_Usuario`, `Nombre_Usuario`, `Correo_Electronico`, `FechaNacimiento` , `Pass`) VALUES ('$dui','$usuario','$correo','$fecNac','$contra')";
+    
+        $finconsulta = mysqli_query($conexion,$query);
+        
+                if ($finconsulta) {
+                    echo "<p>Conexion exitosa</p>";
+        
+                    echo "<p>Dui".$dui."<br>Ususario ".$usuario."<br>Correo".$correo."<br>Fecha de Nacimeinto".$fecNac."<br>Contraseña".$contra."</p>";
+                    header('Location: ../index.php');
+                    exit;
+                }else{
+                    echo "<p>Conexion fallida</p>"; 
+                }
+         }
+    
+  
+}else{
+    // errores
 
-            echo "<p>Dui".$dui."<br>Ususario ".$usuario."<br>Correo".$correo."<br>Fecha de Nacimeinto".$fecNac."<br>Contraseña".$contra."</p>";
-        }else{
-            echo "<p>Conexion fallida</p>"; 
-        }
+
+}
+
 
 ?>
