4ea5a56b9b3a9000578ac91228141c4591403fb3
RonaldoMendo <114121989+RonaldoMendo@users.noreply.github.com>
2023-03-29 21:14:59 -0600

    Cambio diseño main login
    Se ha cambiado el diseño del login principa

--- index.php
+++ index.php
@@ -3,36 +3,36 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Banco De Agricultura</title>
+	
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <!--Barra -->
-<div class="barra">
-	<blockquote><p>Banco De Agricultura</p></blockquote>
-</div>
-<!--agregar nuevo comentario-->
+	<div id="barra">
+		<a class="waves-effect waves-light btn">Banco de agricultura</a>
+	</div>
+	<div class="IniciSec"><h1>Inicio de sesión</h1></div>
+
 <!--Formulario-->
-<div class="wrapper">
-<div class="formulario">
-	<div class="encabezado"><h2>Login</h2><hr></div>
 	<form method="post" action="">	
-	  <label for="usuario">Usuario:</label>
-	  <input type="text" id="usuario"  name="usuario"><br><br>
-	  <label for="contra">Contraseña:</label>
-	  <input type="text" id="contra"  name="contra"><br>
+	<label for="usuario">Usuario</label>
+      <input type="text" id="usuario" name="usuario" placeholder="Ingresa tu usuario" /><br><br>
+	  <label for="contrasena">Contraseña</label>
+      <input type="password" id="contra" name="contra" placeholder="Ingresa tu contraseña" /> 
 	  <?php
-include("base/procesar.php");
-?>
-	  <div class="opciones">	
-	  <a href="recursos/olvidoContra.php" id="btncrear">Olvide contraseña</a>|
-	  <a href="recursos/registro.php">Crear Cuenta</a>
-	  </div>
-	  <input type="submit" name="enviar" value="LOGIN">
-	  
-	</form> 
-	</div>
-	</div>
+		include("base/procesar.php");
+	  ?>
+
+	  <input type="submit" value="Ingresar" />
+      <div class="forgot-password">
+        <a href="#">¿Olvidaste tu contraseña?</a> | <a href="#">Crear usuario</a>
+      </div>
+    </form>
+    <br><br><br><br>
+
+<script src="js/materialize.min.js"></script>
 </body>
 </html>
