2203b4bacb3870a2b014cb9d46460476d580dfe2
Katopy <84346755+Katopy@users.noreply.github.com>
2023-03-31 15:16:30 -0600

    Validaciones de registro1
    por favor revisar si las validaciones funcionan dejando en blanco una de ellas
    
    Tambien se modifco diseño en validaciones.css

--- recursos/registro.php
+++ recursos/registro.php
@@ -7,8 +7,11 @@
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/validaciones.css">
+
 </head>
 <body>
+	<?php include("registro1.php");?>
 <!--Barra -->
 	<div id="barra">
 		<a class="waves-effect waves-light btn ">Banco de agricultura</a>
@@ -17,23 +20,27 @@
 
 	<form action="registro1.php" method="post">	
 	  <label for="usuario">Usuario:</label>
-	  <input type="text" id="usuario"  name="usuario" required><br>
+	  <input type="text" id="usuario"  name="usuario"><br>
 
 	  <label for="contra">Contraseña:</label>
-	  <input type="password" id="contra"  name="contra" required><br>
+	  <input type="password" id="contra"  name="contra">
+	  <?php if(in_array("La contraseña debe tener al menos 8 caracteres.", $error)) { echo "<p class='error'>La contraseña debe tener al menos 8 caracteres.</p>"; } ?><br>
 
 	  <label for="contra">Fecha de nacimiento:
-	  <input type="date" id="contra"  name="fecnac" required> </label><br>
+	  <input type="date" id="contra"  name="fecnac" required> 
+	  <?php if(in_array("El formato de fecha de nacimiento es incorrecto. Debe tener el formato AAAA-MM-DD.", $error)) { echo "<p class='error'>El formato de fecha de nacimiento es incorrecto. Debe tener el formato DD-MM-AAAA.</p>"; } ?>
+	  </label><br>
 
 	  <label for="correos">Correo:
 	  <input type="mail" id="correo"  name="correo" placeholder="nombre@gmail.com"  pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" required></label><br><br>
 
 	  <label for="dui">DUI:</label>		
-	  <input type="text" pattern="\d{8}-\d" id="dui"  name="dui" placeholder="12345678-9" required><br><hr>
+	  <input type="text" pattern="\d{8}-\d" id="dui"  name="dui" placeholder="12345678-9"><br><hr>
 
 	  <input type="submit" value="Registrar" name='submit'>
 	</form> 
-	<br>
+
+	
 </form>
 </body>
 </html>
--- recursos/registro1.php
+++ recursos/registro1.php
@@ -3,7 +3,7 @@
 // viendo el problema de diferentes roles voy a probar una variable de control en la 
 // base de datos
 include("conexion.php");
-
+$error=array();
 //Capturando las variables globales
         if (isset($_POST['dui']) || isset($_POST['usuario']) || isset($_POST['correo']) || isset($_POST['fecnac']) || isset($_POST['contra'])) {
             //Borrando espacios innecesarios
@@ -15,16 +15,26 @@ include("conexion.php");
             $contra =trim($_POST['contra']);
             $contra_encriptada = password_hash($contra, PASSWORD_DEFAULT); //encripta la base de datos
 
+            //Validacion de usuario
+            if(empty($_POST['usuario'])) {
+                $error[] = "El campo de usuario es obligatorio.";
+             }
 
-            //Validacion
-            if(isset($_POST['submit'])){
-                if(empty('dui')){
-                    echo "<p class='error'>Debes ingresar el nombre</p>";
-                }
-            }
+            //Validacion de contra
+             if(empty($_POST['contra'])) {
+                $error[] = "El campo de contraseña es obligatorio.";
+             } elseif(strlen($_POST['contra']) < 8) {
+                $error[] = "La contraseña debe tener al menos 8 caracteres.";
+             }
 
+            //Validacion de fecha
+            if(empty($_POST['fecnac'])) {
+                $error[] = "El campo de fecha de nacimiento es obligatorio.";
+             } elseif(!preg_match("/^\d{2}-\d{2}-\d{4}$/", $_POST['fecnac'])) {
+                $error[] = "El formato de fecha de nacimiento es incorrecto. Debe tener el formato DD-MM-AAAA.";
+             }
+             
 
-    
             if (preg_match('/^[0-9]{8}-[0-9]{1}$/', $dui) || filter_var($correo, FILTER_VALIDATE_EMAIL)) {
                     $query ="INSERT INTO `usuario`(`DUI_Usuario`, `Nombre_Usuario`, `Correo_Electronico`, `FechaNacimiento` , `Pass` , `ID_cuenta`) VALUES ('$dui','$usuario','$correo','$fecNac','$contra_encriptada',1)";
 
@@ -40,10 +50,14 @@ include("conexion.php");
                     }else{
                         echo "<p>Conexion fallida</p>"; 
                     }
-            }else{
-                
-            } 
+                }else{
+
+                echo "Error en el formato.";
+            }
+            
     }else{
              // error falta hacer el error al poner el mismo dui que esta en la base de datos
     }
+
+    
 ?>
